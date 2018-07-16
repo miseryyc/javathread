e03b97372ffb99cde1474a3105cf0579e510ffe2
yangchong <yangchong1011@163.com>
2018-07-16 08:18:27 +0800

    提供更多注释

--- src/com/tjh/concurrent/nio/MultiThreadNIOEchoServer.java
+++ src/com/tjh/concurrent/nio/MultiThreadNIOEchoServer.java
@@ -75,9 +75,11 @@
              // 获取跟当前key相关联的对象
              EchoClient echoClient = (EchoClient) sk.attachment();
              echoClient.enqueue(bb);
+             System.out.println("将读数据插入队列中并设置兴趣到读和写" + System.currentTimeMillis());
              //设置对读和写感兴趣
              sk.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
              //处理完了之后唤醒Selector处理数据。
+             System.out.println("开始唤醒selector" + System.currentTimeMillis());
              selector.wakeup();
          }
      }
@@ -94,10 +96,11 @@
          InetSocketAddress isa = new InetSocketAddress(8000);
          //服务端的ServerSocket绑定ip地址
          ssc.socket().bind(isa);
-         // 注册感兴趣的事件，此处对accept事件感兴趣,将Selector和Channel绑定关联起来
+         //注册感兴趣的事件，此处对accept事件感兴趣,将Selector和Channel绑定关联起来
          ssc.register(selector, SelectionKey.OP_ACCEPT);
          //无限循环监听
          while (true) {
+             System.out.println("开始阻塞等待。" + System.currentTimeMillis());
              int num = selector.select();
              System.out.println("有" + num + "个通道的数据准备好了。" + System.currentTimeMillis());
              //如果没有准备好的Channel则直接进行下一次循环监听
@@ -115,14 +118,14 @@
                  i.remove();
                  //判断acceptable表示客户端连接已经准备好了。
                  if (sk.isAcceptable()) {
-                     System.out.println("连接服务器成功");
+                     System.out.println("连接服务器成功。" + System.currentTimeMillis());
                      //处理accept事件
                      doAccept(sk);
                  }
                  //判断read事件，进行读数据。
                  //每次向选择器注册通道时就会创建一个选择键。通过调用某个键的 cancel 方法、关闭其通道，或者通过关闭其选择器来取消 该键之前，
-                 // 它一直保持有效。取消某个键不会立即从其选择器中移除它；相反，会将该键添加到选择器的已取消键集，以便在下一次进行选择操作时移除它。
-                 // 可通过调用某个键的 isValid 方法来测试其有效性。
+                 //它一直保持有效。取消某个键不会立即从其选择器中移除它；相反，会将该键添加到选择器的已取消键集，以便在下一次进行选择操作时移除它。
+                 //可通过调用某个键的 isValid 方法来测试其有效性。
                  else if (sk.isValid() && sk.isReadable()) {
                      //Channel从客户端读取数据时的开始时间
                      if (!geym_time_stat.containsKey(((SocketChannel) sk
@@ -145,6 +148,7 @@
                      //整个服务端响应客户端的时间是从读channel读数据到写数据到channel这段期间花的时间
                  }
              }
+             Thread.sleep(2000);
          }
      }
 
@@ -153,6 +157,7 @@
       * @param sk SelectionKey
       */
      private void doWrite(SelectionKey sk) {
+         System.out.println("开始写数据。" + System.currentTimeMillis());
          SocketChannel channel = (SocketChannel) sk.channel();
          EchoClient echoClient = (EchoClient) sk.attachment();
          LinkedList<ByteBuffer> outq = echoClient.getOutputQueue();
@@ -160,7 +165,7 @@
          try {
              int len = channel.write(bb);
              if (len == -1) {
-                 disconnect();
+                 disconnect(sk);
                  return;
              }
              //如果数据队列中的数据写完了，则将数据从队列中移除
@@ -168,7 +173,7 @@
                  outq.removeLast();
              }
          } catch (Exception e) {
-             disconnect();
+             disconnect(sk);
          }
          //如果所有的队列数据都没了，则经兴趣设置成读操作。
          if (outq.size() == 0) {
@@ -181,25 +186,26 @@
       * @param sk SelectionKey
       */
      private void doRead(SelectionKey sk) {
+         System.out.println("监听到读操作事件，进行读操作" + System.currentTimeMillis());
          SocketChannel channel = (SocketChannel) sk.channel();
          ByteBuffer bb = ByteBuffer.allocate(8192);
          int len;
          try {
              len = channel.read(bb);
              if (len < 0) {
-                 disconnect();
+                 disconnect(sk);
                  return;
              }
          } catch (Exception e) {
-             disconnect();
+             disconnect(sk);
              return;
          }
          bb.flip();
          tp.execute(new HandleMsg(sk, bb));
      }
 
-     private void disconnect() {
-         //省略略干关闭操作
+     private void disconnect(SelectionKey skey) {
+         skey.cancel();
      }
 
      private void doAccept(SelectionKey sk) {
@@ -208,10 +214,11 @@
          try {
              clientChannel = server.accept();
              clientChannel.configureBlocking(false);
+             System.out.println("客户端连接完毕，注册监听读操作"+ System.currentTimeMillis());
              SelectionKey clientKey = clientChannel.register(selector,
                  SelectionKey.OP_READ);
-             EchoClient echoClinet = new EchoClient();
-             clientKey.attach(echoClinet);
+             EchoClient echoClient = new EchoClient();
+             clientKey.attach(echoClient);
              InetAddress clientAddress = clientChannel.socket().getInetAddress();
              System.out.println("Accepted connection from "
                  + clientAddress.getHostAddress());
